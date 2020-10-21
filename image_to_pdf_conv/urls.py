e1a8e38ac76058b3260501f6e7a7dcea388baa8d
Anish kr singh <65110035+Ankrsi@users.noreply.github.com>
2020-10-21 14:18:03 +0530

    Update urls.py

--- image_to_pdf_conv/urls.py
+++ image_to_pdf_conv/urls.py
@@ -22,7 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.first_page,name='first_page'),
-    path('download/',views.download,name='download'),
 ]
 
 if settings.DEBUG:
