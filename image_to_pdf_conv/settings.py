23e98170d6b003dfa81fcd869de01a46608cc0d1
Anish kr singh <65110035+Ankrsi@users.noreply.github.com>
2020-10-21 14:25:48 +0530

    Update settings.py

--- image_to_pdf_conv/settings.py
+++ image_to_pdf_conv/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=o6=g0#dir2p=f&)l^o(a^7+q3_3cwrcy65+gj!gbj=nuznpr9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['Ankrsi12.pythonanywhere.com']
+ALLOWED_HOSTS = ['image--to--pdf.herokuapp.com']
 
 
 # Application definition
@@ -119,10 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_URL = '/static/'
-#STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-PROJECT_DIR=os.path.dirname(os.path.abspath(__file__))
 
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
